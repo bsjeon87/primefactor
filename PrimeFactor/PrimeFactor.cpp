4bf0f777dcde59752404cdc33806b14936937009
Bongsu Jeon <bs.jeon87@gmail.com>
2025-03-27 15:15:50 +0900

    refactoring

--- PrimeFactor/PrimeFactor.cpp
+++ PrimeFactor/PrimeFactor.cpp
@@ -7,15 +7,20 @@ public:
 		std::vector<int> result = {};
 
 		if (value > 1) {
+			int divisor = 2;
 			if (value == 4) {
-				while (value % 2 == 0) {
-					result.push_back(2);
-					value /= 2;
+				while (value % divisor == 0) {
+					result.push_back(divisor);
+					value /= divisor;
 				}
 			}
 			else if (value == 6) {
-				result.push_back(2);
-				result.push_back(3);
+				for (divisor = 2; value > 1; divisor++) {
+					while (value % divisor == 0) {
+						result.push_back(divisor);
+						value /= divisor;
+					}
+				}
 			}
 			else {
 				result.push_back(value);
